a42bc1a70af4e3d63a95509a78c0a80338d3fa83
stephane <stephane@com.com>
2014-11-14 14:19:26 +0100

    license

--- vcil.m
+++ vcil.m
@@ -2,48 +2,32 @@ clear all
 close all
 clc
 
-% ==========================================================================
-% 
+% =========================================================================
 % Centre National de la Recherche Scientifique (CNRS, France; www.cnrs.fr)
 % Universit? d?Aix Marseille (AMU, France; www.univ-amu.fr)
-% Author : 	Stephane Dufau, 
-% 			Laboratoire de psychologie cognitive,
-% 			stephane.dufau@univ-amu.fr
-% 
-% Creation : Novembre 2014
+% Author: Stephane Dufau, Laboratoire de psychologie cognitive, 
+% stephane.dufau@univ-amu.fr
+% Creation: Novembre 2014
 % 
 % This software is governed by the CeCILL-B license under French law and
 % abiding by the rules of distribution of free software. You can  use, 
-% modify and/ or redistribute the software under the terms of the CeCILL-B
+% modify and/or redistribute the software under the terms of the CeCILL-B
 % license as circulated by CEA, CNRS and INRIA at the following URL
-% "http:% www.cecill.info". 
-% 
-% The fact that you are presently reading this means that you have had
-% knowledge of the CeCILL-B license and that you accept its terms. You can
-% find a copy of this license in the file "License_CeCILL-B_V1-en.txt".
-% 
-% ==========================================================================
-% 
-% Centre National de la Recherche Scientifique (CNRS, France; www.cnrs.fr)
-% Universit? d?Aix Marseille (AMU, France; www.univ-amu.fr)
-% Auteur : 	St?phane Dufau, 
-% 			Laboratoire de psychologie cognitive,
-% 			stephane.dufau@univ-amu.fr
-% 
-% Cr?ation : Novembre 2014
+% "http://www.cecill.info". The fact that you are presently reading this 
+% means that you have had knowledge of the CeCILL-B license and that you 
+% accept its terms. You can find a copy of this license in the file 
+% "License_CeCILL-B_V1-en.txt".
 % 
 % Ce logiciel est r?gi par la licence CeCILL-B soumise au droit fran?ais et
 % respectant les principes de diffusion des logiciels libres. Vous pouvez
 % utiliser, modifier et/ou redistribuer ce programme sous les conditions
 % de la licence CeCILL-B telle que diffus?e par le CEA, le CNRS et l'INRIA 
-% sur le site "http:% www.cecill.info".
-% 
-% Le fait que vous puissiez acc?der ? cet en-t?te signifie que vous avez 
-% pris connaissance de la licence CeCILL-B, et que vous en avez accept? les
-% termes. Vous pouvez trouver une copie de la licence dans le fichier 
-% "Licence_CeCILL-B_V1-fr.txt".
-% 
-% ==========================================================================
+% sur le site "http://www.cecill.info". Le fait que vous puissiez acc?der ?
+% cet en-t?te signifie que vous avez % pris connaissance de la licence 
+% CeCILL-B, et que vous en avez accept? les termes. Vous pouvez trouver une
+% copie de la licence dans le fichier "Licence_CeCILL-B_V1-fr.txt".
+% =========================================================================
+
 
 get_vc
 
